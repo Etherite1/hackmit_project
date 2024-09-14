5e712f95df13b823a3dd8ffead384cd0ef3dd7d4
mschubs <alexsmarcus1980@gmail.com>
2024-09-14 17:02:18 -0400

    added mathjax for parsing latex

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useQuery, useMutation } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { useEffect, useState } from "react";
 import { Button } from "./Button";
+import { MathJaxContext, MathJax } from "better-react-mathjax";
 
 // For demo purposes. In a real app, you'd have real user data.
 const NAME = "You"; 
@@ -26,127 +27,133 @@ export default function App() {
   const [stage, setStage] = useState("user_input");
 
   return (
-    <main className="chat">
-      <header>
-        <h1>Math Helper</h1>
-      </header>
+    <MathJaxContext>
+      <main className="chat">
+        <header>
+          <h1>Math Helper</h1>
+        </header>
 
-      <aside className="stats">
-        <p>Correct Answers: {accuracy?.[0]?.correctAnswers ?? 0}</p>
-        <p>Incorrect Answers: {accuracy?.[0]?.incorrectAnswers ?? 0}</p>
-        <p>
-          Percent Correct:{" "}
-          {(() => {
-            const correct = accuracy?.[0]?.correctAnswers ?? 0;
-            const incorrect = accuracy?.[0]?.incorrectAnswers ?? 0;
-            const total = correct + incorrect;
-            return total > 0
-              ? `${((correct / total) * 100).toFixed(2)}%`
-              : "N/A";
-          })()}
-        </p>
-      </aside>
+        <aside className="stats">
+          <p>Correct Answers: {accuracy?.[0]?.correctAnswers ?? 0}</p>
+          <p>Incorrect Answers: {accuracy?.[0]?.incorrectAnswers ?? 0}</p>
+          <p>
+            Percent Correct:{" "}
+            {(() => {
+              const correct = accuracy?.[0]?.correctAnswers ?? 0;
+              const incorrect = accuracy?.[0]?.incorrectAnswers ?? 0;
+              const total = correct + incorrect;
+              return total > 0
+                ? `${((correct / total) * 100).toFixed(2)}%`
+                : "N/A";
+            })()}
+          </p>
+        </aside>
 
-      {messages?.map((message) => (
-        <article
-          key={message._id}
-          className={message.author === NAME ? "message-mine" : ""}
-        >
-          <div>{message.author}</div>
-          <p>{message.body}</p>
-        </article>
-      ))}
-      {stage == "user_input" && 
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            await sendMessage({ body: newMessageText, author: NAME });
-            setNewMessageText("");
-            setStage("skip_reveal");
-          }}
-        >
-          <input
-            value={newMessageText}
-            onChange={(e) => setNewMessageText(e.target.value)}
-            placeholder="Write a query..."
-            style={{ width: '100%'}}
-          />
-        </form>
-      }
-      {stage == "skip_reveal" &&
-        <div className="button-group">
-          <Button
-            height={3}
-            width={10}
-            text="New Query"
-            onClick={() => {
-              setStage("user_input");
-            }}
-          />
-          <Button
-            height={3}
-            width={10}
-            text="Similar Problem"
-            onClick={() => {
+        {messages?.map((message) => (
+          <article
+            key={message._id}
+            className={message.author === NAME ? "message-mine" : ""}
+          >
+            <div>{message.author}</div>
+            <p>
+              <MathJax>
+                {message.body}
+              </MathJax>
+            </p>
+          </article>
+        ))}
+        {stage == "user_input" && 
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              await sendMessage({ body: newMessageText, author: NAME });
+              setNewMessageText("");
               setStage("skip_reveal");
             }}
-          />
-          <Button
-            height={3}
-            width={10}
-            text="Reveal Answer"
-            onClick={() => {
-              setStage("right_wrong");
-            }}
-          />
-        </div>
-      }
-      {stage == "right_wrong" && 
-        <div className="button-group">
-          <Button
-            height={3}
-            width={10}
-            text="Correct"
-            background_color="green"
-            onClick={() => {
-              updateCorrect();
-              setStage("similar_new")
-            }}
-            className="correct" // Add class for correct button
-          />
-          <Button
-            height={3}
-            width={10}
-            text="Incorrect"
-            background_color="red"
-            onClick={() => {
-              updateIncorrect();
-              setStage("similar_new")
-            }}
-            className="incorrect" // Add class for incorrect button
-          />
-        </div>
-      }
-      {stage == "similar_new" && 
-        <div className="button-group">
-          <Button
-            height={3}
-            width={10}
-            text="Similar Problem"
-            onClick={() => {
-              setStage("skip_reveal");
-            }}
-          />
-          <Button
-            height={3}
-            width={10}
-            text="New query"
-            onClick={() => {
-              setStage("user_input");
-            }}
-          />
-        </div>
-      }
-    </main>
+          >
+            <input
+              value={newMessageText}
+              onChange={(e) => setNewMessageText(e.target.value)}
+              placeholder="Write a query..."
+              style={{ width: '100%'}}
+            />
+          </form>
+        }
+        {stage == "skip_reveal" &&
+          <div className="button-group">
+            <Button
+              height={3}
+              width={10}
+              text="New Query"
+              onClick={() => {
+                setStage("user_input");
+              }}
+            />
+            <Button
+              height={3}
+              width={10}
+              text="Similar Problem"
+              onClick={() => {
+                setStage("skip_reveal");
+              }}
+            />
+            <Button
+              height={3}
+              width={10}
+              text="Reveal Answer"
+              onClick={() => {
+                setStage("right_wrong");
+              }}
+            />
+          </div>
+        }
+        {stage == "right_wrong" && 
+          <div className="button-group">
+            <Button
+              height={3}
+              width={10}
+              text="Correct"
+              background_color="green"
+              onClick={() => {
+                updateCorrect();
+                setStage("similar_new")
+              }}
+              className="correct" // Add class for correct button
+            />
+            <Button
+              height={3}
+              width={10}
+              text="Incorrect"
+              background_color="red"
+              onClick={() => {
+                updateIncorrect();
+                setStage("similar_new")
+              }}
+              className="incorrect" // Add class for incorrect button
+            />
+          </div>
+        }
+        {stage == "similar_new" && 
+          <div className="button-group">
+            <Button
+              height={3}
+              width={10}
+              text="Similar Problem"
+              onClick={() => {
+                setStage("skip_reveal");
+              }}
+            />
+            <Button
+              height={3}
+              width={10}
+              text="New query"
+              onClick={() => {
+                setStage("user_input");
+              }}
+            />
+          </div>
+        }
+      </main>
+    </MathJaxContext>
   );
 }
